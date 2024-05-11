3ce5ce15cf7ee0688f0fdaca75f09051a48c1c3a
sukhrob <suhrobturaev2004@gmail.com>
2024-05-11 23:31:10 +0500

    gitignore

--- avto/apps.py
+++ avto/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AvtoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'avto'
+
+    def ready(self):
+        import avto.signals
